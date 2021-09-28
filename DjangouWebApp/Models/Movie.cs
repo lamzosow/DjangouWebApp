999a51593106eed9818f6aa2ba93ec11e28b3a20
Mama Sow <Salliasow883@gmail.com>
2021-09-28 12:10:36 -0400

    First Commit

---
+++ DjangouWebApp/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace DjangouWebApp.Models
+{
+    public class Movie
+    {
+        public Movie()
+        { }
+            
+
+            public int Id { get; set; }
+
+
+            //[StringLength(50)]
+            public string Title { get; set; }
+
+            public string Genre { get; set; }
+
+            //[DataType(DataType.Date)]
+            public DateTime ReleaseDate { get; set; }
+
+            public decimal Price { get; set; }
+
+        
+          }
+   }
